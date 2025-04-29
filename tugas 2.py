52ecb52cf841d84960c2d09965effdfccef693d1
torixgilang <tourixgilang@gmail.com>
2025-04-29 17:09:51 +0800

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,12 @@
+print ("tourix gilang al farizi 2424113737")
+inputuser = int(input("masukkan bilangan antara 24 atau lebih dari 37"))
+
+#+++++24------------
+#memeriksa angka yang kurang dari 37
+iskurangdari = (inputuser <24)
+
+#memeriksa angka yang kurang dari 37
+islebihdari = (inputuser <37)
+
+final = iskurangdari or islebihdari
+print("angka yang anda masukan", final)
